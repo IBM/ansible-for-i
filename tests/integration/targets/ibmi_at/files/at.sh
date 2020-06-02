3ba6a40062453473e8df2a1b8ed08936f52a222c
root <root@ip9-114-192-95.pok.stglabs.ibm.com>
2020-06-02 03:14:04 -0400

    remove useless files

--- tests/integration/targets/ibmi_at/files/at.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#sleep_time='$1';
-file_name='$1'
-
-echo $(date) >> $1 
--- tests/integration/targets/ibmi_job/files/sbmjob_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sleep_time='$1';
-
-[ ls, -l, / ]
-[ sh, -xc, "echo $(date) ': hello world!'" ]
-[ sh, -c, echo "=========hello world'=========" ]
-echo "hello" > /home/cldtest
-sleep $sleep_time
