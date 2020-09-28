9004151cd5b7c81b9ad46abfb41c9f328f866824
Travis CI <zhuljbj@cn.ibm.com>
2020-09-28 14:44:43 +0000

    Travis build: 3881

--- plugins/modules/ibmi_module_config.py
+++ plugins/modules/ibmi_module_config.py
@@ -123,7 +123,7 @@ import json
 import os
 import pwd
 
-__ibmi_module_version__ = "1.1.1"
+__ibmi_module_version__ = "1.1.2"
 
 
 def main():
--- plugins/modules/ibmi_sysval.py
+++ plugins/modules/ibmi_sysval.py
@@ -150,7 +150,7 @@ try:
 except ImportError:
     HAS_IBM_DB = False
 
-__ibmi_module_version__ = "1.1.1"
+__ibmi_module_version__ = "1.1.2"
 
 
 sysval_array = [
