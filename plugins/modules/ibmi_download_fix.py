9d990954dd62943f9723edaf4a43f8f5a6340b0a
Travis CI <zhuljbj@cn.ibm.com>
2021-06-29 00:43:40 +0000

    Travis build: 5690

--- plugins/modules/ibmi_download_fix.py
+++ plugins/modules/ibmi_download_fix.py
@@ -280,7 +280,7 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.ibm.power_ibmi.plugins.module_utils.ibmi import ibmi_util
 from ansible_collections.ibm.power_ibmi.plugins.module_utils.ibmi import db2i_tools
 from ansible_collections.ibm.power_ibmi.plugins.module_utils.ibmi import ibmi_module as imodule
-__ibmi_module_version__ = "9.9.9"
+__ibmi_module_version__ = "1.4.0"
 
 HAS_ITOOLKIT = True
 
